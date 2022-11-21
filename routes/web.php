f674eecc298a780a481d7871ef27c6be7071284b
Kinslert Virguez <dkinslert@gmail.com>
2022-11-21 09:15:08 -0400

    web routes, home theme inclusion

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Inertia\Inertia;
 |
 */
 
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -23,6 +24,10 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+*/
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
